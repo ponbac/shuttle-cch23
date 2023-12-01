2d9919595454b7930aeebf8131e47e7e7f4ee79f
ponbac <root@ponbac.xyz>
2023-12-01 16:28:21 +0100

    u64

--- src/main.rs
+++ src/main.rs
@@ -19,9 +19,9 @@ async fn main() -> shuttle_axum::ShuttleAxum {
 }
 
 async fn day1_part2(Path(numbers): Path<String>) -> impl IntoResponse {
-    let xor_sum: u32 = numbers
+    let xor_sum: u64 = numbers
         .split('/')
-        .map(|s| s.parse::<u32>().unwrap())
+        .map(|s| s.parse::<u64>().unwrap())
         .fold(0, |acc, n| acc ^ n);
 
     format!("{}", xor_sum.pow(3))
