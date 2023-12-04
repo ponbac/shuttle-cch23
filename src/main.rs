31d1df61c564ac71614e0a076c5b8547cc9510bc
ponbac <root@ponbac.xyz>
2023-12-04 20:17:31 +0100

    day4 part 1

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,11 @@
-use axum::{extract::Path, http::StatusCode, response::IntoResponse, routing::get, Router};
+use axum::{
+    extract::Path,
+    http::StatusCode,
+    response::IntoResponse,
+    routing::{get, post},
+    Json, Router,
+};
+use serde::{Deserialize, Serialize};
 
 async fn hello_world() -> &'static str {
     "Hello, world!"
@@ -13,7 +20,8 @@ async fn main() -> shuttle_axum::ShuttleAxum {
     let router = Router::new()
         .route("/", get(hello_world))
         .route("/-1/error", get(error))
-        .route("/1/*numbers", get(day1_part2));
+        .route("/1/*numbers", get(day1_part2))
+        .route("/4/strength", post(day4_part1));
 
     Ok(router.into())
 }
@@ -27,4 +35,14 @@ async fn day1_part2(Path(numbers): Path<String>) -> impl IntoResponse {
         .fold(0, |acc, n| acc ^ n);
 
     format!("{}", xor_sum.pow(3))
+}
+
+#[derive(Serialize, Deserialize)]
+struct Reindeer {
+    name: String,
+    strength: u32,
+}
+
+async fn day4_part1(Json(reindeers): Json<Vec<Reindeer>>) -> impl IntoResponse {
+    format!("{}", reindeers.iter().map(|r| r.strength).sum::<u32>())
 }
